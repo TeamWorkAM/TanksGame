9b86469153ebc40d3b04049c08fcf29f403b5de8
IndDevComp <brevnov-00234@mail.ru>
2020-06-03 21:54:32 +0600

    Anton

---
+++ Engine/Commands/Rotate.cs
@@ -0,0 +1,30 @@
+﻿using Tanks.Engine.EntityProp;
+
+namespace Tanks.Engine.Commands
+{
+    public class Rotate : Command
+    {
+        Rotatable entity;
+
+        public Rotate(Rotatable rotatable)
+        {
+            entity = rotatable;
+        }
+
+        public void Action()
+        {
+            try
+            {
+                if (entity != null)
+                {
+                    entity.Angle += entity.Velocity;
+                }
+                else throw new CommandException();
+            }
+            catch
+            {
+                
+            }
+        }
+    }
+}
