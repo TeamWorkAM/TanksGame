9c3f206f7e68b7e85180f7c5ac5350141b6ba1bb
IndDevComp <brevnov-00234@mail.ru>
2020-06-03 21:50:53 +0600

    Engine

---
+++ Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class CommandException : Exception
+{
+    public CommandException() : base("Ошибка выполнения команды поворота танка") { }
+}
+
+namespace Tanks.Engine
+{
+    public interface Command
+    {
+        void Action();
+    }
+}
